614b0d56b87de482c1c27537384476962b0c93ba
Florent <florent.lagrede@gmail.com>
2021-03-24 18:47:09 +0100

    add perspective to html overlay

--- src/ui/ItemDescription.js
+++ src/ui/ItemDescription.js
@@ -41,28 +41,30 @@ const ItemDescription = () => {
   }
 
   return (
-    <div className="w-full md:w-1/3 relative md:fixed right-0 px-3 mt-6 md:mt-0">
-      {isTriggered && <SideMenuEffect selectedItem={selectedItem} />}
-      {isTriggered ? <AnimatedOuterBox /> : <OuterBox />}
-      <div className={tw`flex items-center bg-menu-header text-xs h-6 px-2 edge-corner mb-2 -my-2`}>Details</div>
-      <img className="mx-auto my-4" src={`/items/${item?.image}`} alt="" />
-      <div className="mb-2">{item?.name}</div>
-      <div className="border-b border-gray-600 border-opacity-40 border-t mb-4 py-4">
-        <div>Container damage</div>
-        <div className="bg-red-500 bg-opacity-80 border border-gray-600 h-4 w-full">
-          {isTriggered ? (
-            <AnimatedBar damage={item.damage} />
-          ) : (
-            <div className="bg-blue-500 bg-opacity-80 h-full w-full" style={{ width: `${item.damage}%` }} />
-          )}
+    <div className="w-full md:w-1/3 relative md:fixed right-0 mt-6 md:mt-0">
+      <div className="w-2/3 relative px-3">
+        {isTriggered && <SideMenuEffect selectedItem={selectedItem} />}
+        {isTriggered ? <AnimatedOuterBox /> : <OuterBox />}
+        <div className={tw`flex items-center bg-menu-header text-xs h-6 px-2 edge-corner mb-2 -my-2`}>Details</div>
+        <img className="mx-auto my-4" src={`/items/${item?.image}`} alt="" />
+        <div className="mb-2">{item?.name}</div>
+        <div className="border-b border-gray-600 border-opacity-40 border-t mb-4 py-4">
+          <div>Container damage</div>
+          <div className="bg-red-500 bg-opacity-80 border border-gray-600 h-4 w-full">
+            {isTriggered ? (
+              <AnimatedBar damage={item.damage} />
+            ) : (
+              <div className="bg-blue-500 bg-opacity-80 h-full w-full" style={{ width: `${item.damage}%` }} />
+            )}
+          </div>
         </div>
-      </div>
-      <div className="mb-2 text-xs">{item?.description}</div>
-      <div
-        className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 cursor-pointer flex items-center justify-center text-white pt-1.5"
-        onClick={handleLike}
-      >
-        Like
+        <div className="mb-2 text-xs">{item?.description}</div>
+        <div
+          className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 cursor-pointer flex items-center justify-center text-white pt-1.5"
+          onClick={handleLike}
+        >
+          Like
+        </div>
       </div>
     </div>
   )
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,11 +1,18 @@
-import { tw } from 'twind'
+import { tw, css } from 'twind/css'
 import Footer from './Footer'
 import Header from './Header'
 import Inventory from './Inventory'
 import ItemDescription from './ItemDescription'
 
 const HtmlOverlay = () => (
-  <div className={tw`absolute bg-black bg-opacity-10 inset-0 pt-20 text-menu-text z-10`}>
+  <div
+    className={tw(
+      css`
+        @apply absolute inset-0 pt-20 md:px-14 text-menu-text z-10;
+        transform: rotate3d(0, 1, 0, 357deg);
+      `
+    )}
+  >
     <Header />
     <div className="flex flex-col md:flex-row h-4/6 overflow-y-scroll no-scrollbar focus:outline-none">
       <Inventory />
